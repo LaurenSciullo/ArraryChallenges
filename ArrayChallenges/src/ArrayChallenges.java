dc5eb706f904c7d7202ca1c47ba51b12dd10efd5
lauren.sciullo <lauren.sciullo@MU-C1-06.mullen.pvt.k12.co.us>
2019-09-06 11:52:21 -0600

    finished sweets challenge

--- ArrayChallenges/src/ArrayChallenges.java
+++ ArrayChallenges/src/ArrayChallenges.java
@@ -6,23 +6,43 @@ public class ArrayChallenges
 		public static void main(String[] args)
 			{
 				Scanner userInput = new Scanner(System.in);
+				Scanner userInput2 = new Scanner(System.in);
 			System.out.println("Hello! How many favorite sweets do you have?");
 				
 			int numberOfSweets = userInput.nextInt();
-			
 		
-			String [] printSweets = new String[numberOfSweets];
 			
-			printSweets [numberOfSweets] = userInput.next();
+			String[] printSweets = new String[numberOfSweets];
 			
-					for (String p : printSweets)
+					for (int i =0; i < numberOfSweets ; i++)
 						{
 							
-					System.out.println("Enter your sweet: ");
+					System.out.println("Enter your sweet: " );
+					String userSweets = userInput2.nextLine();
 					
-					System.out.println("Ahhh your, " + numberOfSweets + " favorite sweet: " + p);
+					printSweets [i] = userSweets;
+					}
+					
+					if (numberOfSweets ==1)
+						{
+							System.out.println("Your sweet: ");
+							for (String p: printSweets)
+								{
+							
+									System.out.println(p);	
+								}
 						}
-						
+					else 
+						{
+					System.out.println("Your sweets: ");
+					for (String p: printSweets)
+						{
+					
+							System.out.println(p);	
+						}
+					
+						}
+					
 			}
 
 	}
