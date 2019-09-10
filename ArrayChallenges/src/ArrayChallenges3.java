52e8e7870d7545a532785f45a5f7b34beb7f8cfb
lauren.sciullo <lauren.sciullo@MU-C1-06.mullen.pvt.k12.co.us>
2019-09-10 09:05:04 -0600

    added syso

--- ArrayChallenges/src/ArrayChallenges3.java
+++ ArrayChallenges/src/ArrayChallenges3.java
@@ -4,6 +4,7 @@ public class ArrayChallenges3 {
 	public static void main(String[] args) {
 		
 		int[] printNumbers = new int[5];
+		
 		System.out.println("Your 5 random numbers: ");
 		for (int p: printNumbers)
 		{
@@ -15,6 +16,11 @@ public class ArrayChallenges3 {
 			
 		}
 		
+		System.out.println("\nYour 5 random numbers backwords: ");
+		for (int i = 5; i >= 0; i--)
+			{
+				System.out.println(printNumbers);
+			}
 		
 	}
 
